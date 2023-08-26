8fa335309053b69e9b93c21ab48bf067c3979f4e
Acrylic125 <acrylic125email@gmail.com>
2023-08-26 17:51:50 +0800

    chore: add app directory

---
+++ src/app/layout.tsx
@@ -0,0 +1,8 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+import "~/styles/globals.css";
+
+const MyApp = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
+
+export default MyApp;

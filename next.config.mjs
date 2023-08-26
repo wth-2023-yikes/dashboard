1a8b51c7fcba11596cc26619ee722b81801416da
Acrylic125 <acrylic125email@gmail.com>
2023-08-27 00:42:37 +0800

    fix: cors

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,26 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  // CORS https://blog.logrocket.com/using-cors-next-js-handle-cross-origin-requests/
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;

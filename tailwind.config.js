2cb64ca47121480083fc48cfc3d6ccbd2f925fd5
Acrylic125 <acrylic125email@gmail.com>
2023-08-27 00:43:27 +0800

    chore: add tailwind base configs

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {},
+};
